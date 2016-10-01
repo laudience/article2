55ef94b2c18b55308072d6995e5ab3d9a1565652
Sébastien Ohleyer <sebastienohleyer@pc45.home>
2016-10-01 16:55:01 +0200

    download of community datas and resources data by creation of a zipfile. Save in json files

--- datagouv_api.py
+++ datagouv_api.py
@@ -1,37 +1,54 @@
-from urllib.request import urlopen
+import requests
 import json
+from zipfile import ZipFile
+from utilitaires import *
+
 
 class GetData:
 
-    def __init__(self, ID):
-        self.url = ''.join(['http://www.data.gouv.fr/api/1/datasets/', ID, '/full/'])
+    def __init__(self, id):
+        self.url = ''.join(['http://www.data.gouv.fr/api/1/datasets/', id, '/full/'])
         self.dataset_url = None
         self.dataset = None
 
-        self.get_dataset_url()
-        self.get_dataset()
+
+    def get_community_dataset_url(self):
+        response = requests.get(self.url).json()
+
+        self.dataset_url = response['community_resources'][0]['url']
+        print(self.dataset_url)
+
+    def get_community_dataset(self):
+        self.get_community_dataset_url()
+        response = requests.get(self.dataset_url).json()
+
+        self.dataset = response
+
+    def save_community_dataset(self, outfile_name):
+        self.get_community_dataset()
+        outfile_directory = ''.join(['./datas/', outfile_name, '.json'])
+        with open(outfile_directory, 'w') as outfile:
+            json.dump(self.dataset, outfile)
 
 
-    def get_dataset_url(self):
-        response = urlopen(self.url)
-        string = response.read().decode('utf-8')
-        json_obj = json.loads(string)
+    def get_zip_dataset_url(self):
+        response = requests.get(self.url).json()
 
-        self.dataset_url = json_obj['community_resources'][0]['url']
+        self.dataset_url = response['resources'][1]['url']
+        print(self.dataset_url)
 
+    def get_dataset_from_zip(self, zipname):
+        self.get_zip_dataset_url()
+        zip_path = ''.join(['./datas/zipfile/', zipname, '.zip'])
+        download_zip(self.dataset_url, zip_path)
 
-    def get_dataset(self):
-        response = urlopen(self.dataset_url)
-        string = response.read().decode('utf-8')
-
-        self.dataset = json.loads(string)
-
-
-
+        zipfile = ZipFile(zip_path)
+        zipfile.extractall(path='./datas')
+        file_list = zipfile.namelist()
+        self.dataset = ZipFile(zip_path).read(file_list[0])
 
 def main():
-    print(GetData('53699934a3a729239d2051a1').dataset_url)
-    print(GetData('53699934a3a729239d2051a1').dataset)
+    GetData('5588362ac751df7971a453bb').get_dataset_from_zip('reunion')
 
 if __name__ == '__main__':
     main()
